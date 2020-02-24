97faeb9bd39d5a2b84ada46c64f4aad881e8ddcc
xxx <1026737316@qq.com>
2020-02-24 18:28:39 +0800

    golang init

--- api/main.go
+++ api/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"go_hh_DoubleKill/api/config"
+	// "go_hh_DoubleKill/api/config"
 	"github.com/gin-gonic/gin"
 )
 
-func main()  {
-    // Engin
+func main() {
+	// Engin
 	//router := gin.Default()
 	// 测试路由
 	r := gin.Default()
@@ -19,45 +19,45 @@ func main()  {
 		})
 	})
 
-    router := gin.New()
-        // 加载html文件，即template包下所有文件
-    router.LoadHTMLGlob("template/*")
-    router.GET("/hello", hello)
-    // 路由组
-    user := router.Group("/user"){   
-		// 请求参数在请求路径上
-        user.GET("/get/:id/:username",controller.QueryById)
-        user.GET("/query",controller.QueryParam)
-        user.POST("/insert",controller.InsertNewUser)
-        user.GET("/form",controller.RenderForm)// 跳转html页面
-        user.POST("/form/post",controller.PostForm)
-        //可以自己添加其他，一个请求的路径对应一个函数
+	// router := gin.New()
+	//     // 加载html文件，即template包下所有文件
+	// router.LoadHTMLGlob("template/*")
+	// router.GET("/hello", hello)
+	// // 路由组
+	// user := router.Group("/user"){
+	// 	// 请求参数在请求路径上
+	//     user.GET("/get/:id/:username",controller.QueryById)
+	//     user.GET("/query",controller.QueryParam)
+	//     user.POST("/insert",controller.InsertNewUser)
+	//     user.GET("/form",controller.RenderForm)// 跳转html页面
+	//     user.POST("/form/post",controller.PostForm)
+	//     //可以自己添加其他，一个请求的路径对应一个函数
 
-        // ...
-    }
+	//     // ...
+	// }
 
-    file := router.Group("/file"){
-        // 跳转上传文件页面
-        file.GET("/view",controller.RenderView) // 跳转html页面
-        // 根据表单上传
-        file.POST("/insert",controller.FormUpload)
-        file.POST("/multiUpload",controller.MultiUpload)
-        // base64上传
-        file.POST("/upload",controller.Base64Upload)
-    }
+	// file := router.Group("/file"){
+	//     // 跳转上传文件页面
+	//     file.GET("/view",controller.RenderView) // 跳转html页面
+	//     // 根据表单上传
+	//     file.POST("/insert",controller.FormUpload)
+	//     file.POST("/multiUpload",controller.MultiUpload)
+	//     // base64上传
+	//     file.POST("/upload",controller.Base64Upload)
+	// }
 
-	// 加载配置文件
-	var err error
-    fPath, _ := os.Getwd()
-    fPath = path.Join(fPath, "conf")
-    configPath := flag.String("c", fPath, "config file path")
-    flag.Parse()
-    err = system.LoadConfigInformation(*configPath)
-    fmt.Printf("%+v\n",common.ConfigInfo.Server)
-    if err != nil {
-        return
-	}
+	// // 加载配置文件
+	// var err error
+	// fPath, _ := os.Getwd()
+	// fPath = path.Join(fPath, "conf")
+	// configPath := flag.String("c", fPath, "config file path")
+	// flag.Parse()
+	// err = system.LoadConfigInformation(*configPath)
+	// fmt.Printf("%+v\n",common.ConfigInfo.Server)
+	// if err != nil {
+	//     return
+	// }
 
 	// 指定地址和端口号
-    router.Run(":8090")
+	r.Run(":8090")
 }
