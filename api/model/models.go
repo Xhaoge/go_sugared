b43d20f4503cd5ed9aeda6a164252700837d7cae
xiaohaogge <1026737316@qq.com>
2020-03-01 19:50:16 +0800

    update model

--- api/model/models.go
+++ api/model/models.go
@@ -5,7 +5,7 @@ package model
 type RoomBaseResponse struct {
 	Code int               `json:"code"`
 	Msg  string            `json:"msg`
-	Data map[string]string `json:data,omitempty`
+	Data map[string]interface `json:data,omitempty`
 }
 
 type UserLoginAdd struct {
