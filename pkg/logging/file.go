0dd851325b5425e786700ecb6a518c1f55f0f1fe
xiaohaogge <1026737316@qq.com>
2020-03-15 17:18:20 +0800

    "add log"

---
+++ pkg/logging/file.go
@@ -0,0 +1,48 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	LogSavePath = "runtime/logs/"
+	LogSaveName = "log"
+	LogFileExt  = "log"
+	TimeFormat  = "20200315165345"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
+
+func getLogFileFullPath() string {
+	prefixPath := getLogFilePath()
+	suffixPath := fmt.Sprintf("%s%s.%s", LogSaveName, time.Now().Format(TimeFormat), LogFileExt)
+	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+}
+
+func openLogFile(filePath string) *os.File {
+	_, err := os.Stat(filePath)
+	switch {
+	case os.IsNotExist(err):
+		mlDir()
+	case os.IsPermission(err):
+		log.Fatalf("Perssion: %v", err)
+	}
+	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("fail to openfile :%v", err)
+	}
+	return handle
+}
+
+func mlDir() {
+	dir, _ := os.Getwd()
+	err := os.MkdirAll(dir+"/"+getLogFilePath(), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
