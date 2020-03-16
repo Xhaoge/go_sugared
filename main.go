f04ff62f92bd8e95e8113abe1004954260b510fb
xxx <1026737316@qq.com>
2020-03-16 19:14:46 +0800

    “add mongo user”

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"go_sugared/config"
-	//"go_sugared/models"
+	"go_sugared/models"
 
 	"go_sugared/routers"
 	"net/http"
@@ -12,11 +12,20 @@ import (
 func main() {
 	fmt.Println("go sugared running.............")
 	config.InitConfig()
-	//models.InitMongo()
+	models.InitMongo()
 	router := routers.InitRouter()
 	s := &http.Server{
 		Addr:    fmt.Sprintf(":%s", config.Setting.Server.Port),
 		Handler: router,
+	}
+	data1 := models.WxUser{
+		Name:     "zhanan",
+		Password: "12356",
+	}
+	// 插入数据
+	cErr := models.UserMgo.Insert(data1)
+	if cErr != nil {
+		fmt.Println(cErr)
 	}
 
 	s.ListenAndServe()
